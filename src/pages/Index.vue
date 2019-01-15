739e793052e3aff788462a3e844e7dee5631f73e
Eric Gorr <ericgorr@gmail.com>
2019-01-15 10:08:49 -0500

    mostly working, but the height is wrong for the scroll area

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,34 @@
 <template>
-  <q-page class="flex">
+  <q-page class="page-content">
 
-    <q-tabs animated swipable color="tertiary" glossy align="left">
+    <div class="title-content">
+      title content
+    </div>
 
-      <q-tab default name="taba" slot="title" label="TabA" />
-      <q-tab name="tabb" slot="title" label="TabB" />
+    <div class="main">
+      <q-tabs animated swipable color="tertiary" glossy align="left">
 
-      <q-tab-pane keep-alive name="taba">
-        <q-scroll-area class="scroll-area">
-          <div v-for="n in 10" :key="n.id">
-            the quick brown fox jumped over the lazy dog
-          </div>
-        </q-scroll-area>
-      </q-tab-pane>
+        <q-tab default name="taba" slot="title" label="TabA" />
+        <q-tab name="tabb" slot="title" label="TabB" />
 
-      <q-tab-pane keep-alive name="tabb">
-        <div>tab b content</div>
-      </q-tab-pane>
+        <q-tab-pane keep-alive name="taba">
+          <q-scroll-area class="scroll-area">
+            <div v-for="n in 100" :key="n.id">
+              the quick brown fox jumped over the lazy dog
+            </div>
+          </q-scroll-area>
+        </q-tab-pane>
 
-    </q-tabs>
+        <q-tab-pane keep-alive name="tabb">
+          <div>tab b content</div>
+        </q-tab-pane>
+
+      </q-tabs>
+    </div>
+
+    <div class="key-content">
+      key content
+    </div>
   </q-page>
 </template>
 
@@ -48,8 +58,27 @@ export default {
 
 <style>
 
+.page-content {
+  background-color: lightgray;
+
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+.main {
+  flex-grow: 1;
+  background-color: green;
+}
+
+.title-content, .main, .key-content {
+  flex-shrink: 0;
+}
+
 .scroll-area {
   width: 100%;
-  height: 500px;
+  height: 100px;
+  background-color: yellow;
 }
 </style>
