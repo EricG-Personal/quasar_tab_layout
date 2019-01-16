7e337e1fb54ad353ab1974c193b0e34d8f3ae821
Eric Gorr <ericgorr@gmail.com>
2019-01-16 08:29:23 -0500

    removed obsolete code

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,27 +7,6 @@
       </div>
     </q-scroll-area>
 
-    <!-- <div class="main">
-      <q-tabs animated swipable color="tertiary" glossy align="left">
-
-        <q-tab default name="taba" slot="title" label="TabA" />
-        <q-tab name="tabb" slot="title" label="TabB" />
-
-        <q-tab-pane keep-alive name="taba">
-          <q-scroll-area class="scroll-area">
-            <div v-for="n in 100" :key="n.id">
-              the quick brown fox jumped over the lazy dog
-            </div>
-          </q-scroll-area>
-        </q-tab-pane>
-
-        <q-tab-pane keep-alive name="tabb">
-          <div>tab b content</div>
-        </q-tab-pane>
-
-      </q-tabs>
-    </div> -->
-
   </q-page>
 </template>
 
@@ -53,31 +32,12 @@ export default {
 
 .page-content {
   background-color: green;
-/*
-  min-height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: stretch; */
 }
-
-.main {
-  flex-grow: 1;
-  background-color: green;
-}
-
-.title-content, .main, .key-content {
-  flex-shrink: 0;
-}
-
 .scroll-area {
-  /* width: 100%;
-  height: 100px; */
   background-color: red;
 }
 
 .scroll-row-content {
-  /* width: 100%;
-  height: 100px; */
   background-color: blue;
   color: white;
 }
